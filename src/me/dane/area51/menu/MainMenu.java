43fcb1ed07ded8a7631c8825e0b4b22e99b1d4f8
Danelegend <43459662+Danelegend@users.noreply.github.com>
2019-08-25 21:31:34 +1000

    Update MainMenu.java

--- src/me/dane/area51/menu/MainMenu.java
+++ src/me/dane/area51/menu/MainMenu.java
@@ -6,7 +6,6 @@ import java.awt.Graphics;
 
 import javax.swing.JFrame;
 
-import me.dane.area51.ScreenHandler;
 import me.dane.area51.Window;
 import me.dane.area51.events.ButtonClickEvent;
 
@@ -14,19 +13,17 @@ public class MainMenu extends Menu {
 	private static final long serialVersionUID = 1L;
 	
 	private JFrame game;
-	private ScreenHandler sh;
 	private Window w;
 	
-	public MainMenu(JFrame game, ScreenHandler sh, Window w) {	
+	public MainMenu(JFrame game, Window w) {	
 		this.game = game;
-		this.sh = sh;
 		this.w = w;
 		inMainMenu();
 	}
 	
 	private void inMainMenu() {
 		setBackground("MainMenuBackground.jpg");
-		addMouseListener(new ButtonClickEvent(game, sh, w));
+		addMouseListener(new ButtonClickEvent(game, w));
 	}
 	
 	@Override
@@ -34,8 +31,8 @@ public class MainMenu extends Menu {
 		super.paintComponent(g);
 		
 		drawTitle(g, 75, "Area51", Color.CYAN, "Comic Sans MS", 70, Font.PLAIN);
-		drawButton(g, 160, 500, 100, 700, Color.RED, "Start Game", Color.BLACK, "Comic Sans MS", 70, Font.PLAIN);
-		drawButton(g, 160, 650, 100, 700, Color.RED, "Exit", Color.BLACK, "Comic Sans MS", 70, Font.PLAIN);
+		drawButton(g, 160, 500, 100, 700, Color.RED, "test", Color.BLACK, "Comic Sans MS", 70, Font.PLAIN);
+		drawButton(g, 160, 650, 100, 700, Color.RED, "test2", Color.BLACK, "Comic Sans MS", 70, Font.PLAIN);
 	}
 	
 }
